f30123dde9e93433a098b5c1ce1613f85c71a5ab
jaesukdo1986 <57671675+jaesukdo1986@users.noreply.github.com>
2025-02-25 00:02:05 +0700

    Delete modules directory

--- modules/api/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "lambda_authorizer_filename" {
-  description = "The filename of the Lambda authorizer zip file."
-  type        = string
-}
-
-variable "region" {
-    type = string
-    default = "ap-southeast-1"
-}
